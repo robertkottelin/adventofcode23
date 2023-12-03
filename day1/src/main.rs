bc3ee20b4b2f81dc702c18cca76c3d5192b43983
Robert Kottelin <robert.kottelin@gmail.com>
2023-12-03 21:01:27 +0100

    Update main.rs

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,27 +1,81 @@
-extern crate common;
-use common::file::read_lines;
-use std::io;
+use std::fs;
 
-// Part 1
-fn main() -> io::Result<()> {
-    let lines = read_lines("data.txt")?;
-
-    let sum = lines.iter()
-        .filter_map(|line| {
-            let chars: Vec<char> = line.chars().collect();
-            if let (Some(first_digit), Some(last_digit)) = (chars.iter().find(|&&c| c.is_digit(10)), chars.iter().rev().find(|&&c| c.is_digit(10))) {
-                let value = first_digit.to_string() + &last_digit.to_string();
-                value.parse::<i32>().ok()
-            } else {
-                None
-            }
+fn main() {
+    // Read the input from the file
+    let input = fs::read_to_string("data.txt").expect("Failed to read file");
+    let lines = input.lines();
+    let part1: u32 = lines
+        .clone()
+        .map(|line| {
+            let left = line
+                .chars()
+                .find(is_digit)
+                .expect("no number found in line");
+            let right = line
+                .chars()
+                .rev()
+                .find(is_digit)
+                .expect("no number found in line");
+            let left = left as u32 - '0' as u32;
+            let right = right as u32 - '0' as u32;
+            left * 10 + right
         })
-        .sum::<i32>();
+        .sum();
+    dbg!(part1);
 
-    println!("Total sum of calibration values: {}", sum);
+    let numbers = vec![
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
 
-    Ok(())
+    let part2: u32 = lines
+        .map(|l| {
+            let mut l = l.to_string();
+            for (index, number) in numbers.iter().enumerate() {
+                l = l.replace((index as u8 + '0' as u8) as char, number);
+            }
+            let mut r = l.clone();
+            let left = loop {
+                let n = match l {
+                    line if line.starts_with("one") => 1,
+                    line if line.starts_with("two") => 2,
+                    line if line.starts_with("three") => 3,
+                    line if line.starts_with("four") => 4,
+                    line if line.starts_with("five") => 5,
+                    line if line.starts_with("six") => 6,
+                    line if line.starts_with("seven") => 7,
+                    line if line.starts_with("eight") => 8,
+                    line if line.starts_with("nine") => 9,
+                    _ => {
+                        l.remove(0);
+                        continue;
+                    }
+                };
+                break n;
+            };
+            let right = loop {
+                let n = match r {
+                    line if line.ends_with("one") => 1,
+                    line if line.ends_with("two") => 2,
+                    line if line.ends_with("three") => 3,
+                    line if line.ends_with("four") => 4,
+                    line if line.ends_with("five") => 5,
+                    line if line.ends_with("six") => 6,
+                    line if line.ends_with("seven") => 7,
+                    line if line.ends_with("eight") => 8,
+                    line if line.ends_with("nine") => 9,
+                    _ => {
+                        r.pop();
+                        continue;
+                    }
+                };
+                break n;
+            };
+            left * 10 + right
+        })
+        .sum();
+    dbg!(part2);
 }
 
-// Part 2
-// ??
+fn is_digit(c: &char) -> bool {
+    c >= &'0' && c <= &'9'
+}
