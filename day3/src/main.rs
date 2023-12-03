2c8c5f80061506f5dadabc889423477b9f3ecb85
Robert Kottelin <robert.kottelin@gmail.com>
2023-12-03 20:34:41 +0100

    Update main.rs

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,15 +1,47 @@
-fn sum_part_numbers(schematic: &[&str]) -> i32 {
+use std::collections::HashSet;
+
+fn sum_part_numbers(schematic: &[String]) -> i32 {
     let mut sum = 0;
+    let mut counted = HashSet::new();
     let rows = schematic.len();
     let cols = schematic[0].len();
-    let mut visited = vec![vec![false; cols]; rows];
 
-    for y in 0..rows {
-        for x in 0..cols {
-            if !visited[y][x] && schematic[y].chars().nth(x).unwrap().is_digit(10) {
-                let number = extract_number(&schematic, &mut visited, y, x);
-                if is_adjacent_to_symbol(&schematic, y, x) {
-                    sum += number;
+    let is_symbol = |c: char| !c.is_digit(10) && c != '.';
+
+    for r in 0..rows {
+        for c in 0..cols {
+            // Check if the current character is a digit and not already counted
+            if schematic[r].as_bytes()[c].is_ascii_digit() && !counted.contains(&(r, c)) {
+                let mut adjacent_to_symbol = false;
+
+                // Check all adjacent cells for a symbol
+                for dr in -1..=1 {
+                    for dc in -1..=1 {
+                        let new_r = r as i32 + dr;
+                        let new_c = c as i32 + dc;
+
+                        if new_r >= 0 && new_r < rows as i32 && new_c >= 0 && new_c < cols as i32 {
+                            if is_symbol(schematic[new_r as usize].as_bytes()[new_c as usize] as char) {
+                                adjacent_to_symbol = true;
+                            }
+                        }
+                    }
+                }
+
+                if adjacent_to_symbol {
+                    // If adjacent to a symbol, parse the entire number
+                    let mut num_str = String::new();
+                    let mut cc = c;
+
+                    while cc < cols && schematic[r].as_bytes()[cc].is_ascii_digit() {
+                        num_str.push(schematic[r].as_bytes()[cc] as char);
+                        counted.insert((r, cc)); // Mark this digit as counted
+                        cc += 1;
+                    }
+
+                    if let Ok(num) = num_str.parse::<i32>() {
+                        sum += num;
+                    }
                 }
             }
         }
@@ -18,55 +50,20 @@ fn sum_part_numbers(schematic: &[&str]) -> i32 {
     sum
 }
 
-fn extract_number(schematic: &[&str], visited: &mut Vec<Vec<bool>>, y: usize, x: usize) -> i32 {
-    let mut number = 0;
-    let mut x_pos = x;
-
-    while x_pos < schematic[y].len() && schematic[y].chars().nth(x_pos).unwrap().is_digit(10) {
-        visited[y][x_pos] = true;
-        number = number * 10 + schematic[y].chars().nth(x_pos).unwrap().to_digit(10).unwrap() as i32;
-        x_pos += 1;
-    }
-
-    number
-}
-
-fn is_adjacent_to_symbol(schematic: &[&str], y: usize, x: usize) -> bool {
-    let rows = schematic.len();
-    let cols = schematic[0].len();
-
-    for dy in -1..=1 {
-        for dx in -1..=1 {
-            if dy == 0 && dx == 0 { continue; }
-
-            let ny = y as i32 + dy;
-            let nx = x as i32 + dx;
-
-            if ny >= 0 && ny < rows as i32 && nx >= 0 && nx < cols as i32 {
-                let adjacent_char = schematic[ny as usize].chars().nth(nx as usize).unwrap();
-                if adjacent_char != '.' && !adjacent_char.is_digit(10) {
-                    return true;
-                }
-            }
-        }
-    }
-
-    false
-}
-
 fn main() {
-    let schematic = [
-        "467..114..",
-        "...*......",
-        "..35..633.",
-        "......#...",
-        "617*......",
-        ".....+.58.",
-        "..592.....",
-        "......755.",
-        "...$.*....",
-        ".664.598.."
+    let schematic = vec![
+        "467..114..".to_string(),
+        "...*......".to_string(),
+        "..35..633.".to_string(),
+        "......#...".to_string(),
+        "617*......".to_string(),
+        ".....+.58.".to_string(),
+        "..592.....".to_string(),
+        "......755.".to_string(),
+        "...$.*....".to_string(),
+        ".664.598..".to_string(),
     ];
 
-    println!("Sum of part numbers: {}", sum_part_numbers(&schematic));
+    let result = sum_part_numbers(&schematic);
+    println!("Sum of part numbers: {}", result);
 }
